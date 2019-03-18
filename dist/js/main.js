48b22372d4b05837b05223ab018ad8b0ac062640
cr50.john <cr50.john@enfrausaph.com>
2019-03-19 04:55:41 +0800

    In Progress

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,33 +1,64 @@
-//Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
-const navItems = document.querySelectorAll ('.nav-item');
+//Set Initial state of the menu
 
-// Set Initial State of Menu
 let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu(){
-        if(!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('close');
-        navItems.forEach(item=>item.classList.add('show'));
+  if(!showMenu){
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
 
-        //Set Menu State
-        showMenu = true;
-        } else {
-            menuBtn.classList.remove('close');
-            menu.classList.remove('show');
-            menuNav.classList.remove('show');
-            menuBranding.classList.remove('close');
-            navItems.forEach(item=>item.classList.remove('show'));
-    
-            //Set Menu State
-            showMenu = false;
+    //Set the menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    //Set the menu state
+    showMenu = false;
+  }
+}
+
+window.onload = function() {
+  Particles.init({
+    selector: ".background",
+    maxParticles: 150,
+    color: "#64ffda",
+    connectParticles: true,
+    sizeVariations: 6,
+    minDistance: 120,
+    responsive: [
+      {
+        breakpoint: 1170,
+        maxParticles: 100,
+        connectParticles: false
+      },
+      {
+        breakpoint: 768,
+        options: {
+          maxParticles: 90,
+          connectParticles: false
         }
-    }
+      },
+      {
+        breakpoint: 500,
+        options: {
+          maxParticles: 100,
+          connectParticles: false
+        }
+      }
+    ]
+  });
+};
